365efda0fa72dcbbe1d11a8005cd8a3891409cce
btrizayade <biaayade@hotmail.com>
2026-08-05 18:51:16 -0300

    feat: create book repository

--- app/repositories/book_repository.py
+++ app/repositories/book_repository.py
@@ -1,3 +1,9 @@
 from sqlalchemy.orm import Session
 
 from app.models.book import Book
+from app.schemas.book import BookResponse
+
+class BookRepository:
+
+    def save(self, db: Session, book: BookResponse):
+        pass
